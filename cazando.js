06881f7814b0ca304a52cb148d146127d9d2c09f
ZahidN09 <zahid09nazateej@gmail.com>
2026-09-09 18:20:40 -0500

    Funcion graficarComida() dibuja un cuadrado en la esquina superior izquierda del canvas, Funcion iniciarJuego() grafica el gato y la comida

--- cazando.js
+++ cazando.js
@@ -5,3 +5,13 @@ function graficarGato(){
     cxt.fillStyle ="#dda02f" ;
     cxt.fillRect(canvas.width/2-70,canvas.height/2-60,140,120);
 }
+
+function graficarComida(){
+    cxt.fillStyle ="#2c9937" ;
+    cxt.fillRect(0,0,80,80);
+}
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
+}
