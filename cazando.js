da1a3d2940a42280d51851669a006bab211ef859
ZahidN09 <zahid09nazateej@gmail.com>
2026-09-09 18:32:07 -0500

    Agregar constantes globales para las funciones de graficar

--- cazando.js
+++ cazando.js
@@ -1,17 +1,31 @@
 let canvas = document.getElementById("areaJuego");
 let cxt = canvas.getContext("2d");
 
-function graficarGato(){
-    cxt.fillStyle ="#dda02f" ;
-    cxt.fillRect(canvas.width/2-70,canvas.height/2-60,140,120);
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+const ALTO_GATO = 130;
+const ANCHO_GATO = 150;
+const ALTO_COMIDA = 60;
+const ANCHO_COMIDA = 60;
+
+function graficarGato() {
+    cxt.fillStyle = "#dda02f";
+    cxt.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
 }
 
-function graficarComida(){
-    cxt.fillStyle ="#2c9937" ;
-    cxt.fillRect(0,0,80,80);
+function graficarComida() {
+    cxt.fillStyle = "#2c9937";
+    cxt.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
 }
 
-function iniciarJuego(){
+function iniciarJuego() {
+    gatoX = (canvas.width - ANCHO_GATO)/2;
+    gatoY = (canvas.height - ALTO_GATO)/2;
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
     graficarGato();
     graficarComida();
 }
